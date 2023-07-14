11d07847180e4b078eb99342c57fadbc8aed8ff5
Arbab Naved <81221340+Hajinaved@users.noreply.github.com>
2023-07-14 20:16:14 +0530

    starting point

---
+++ src/test/java/com/haji/Restfulwebservice/RestfulWebServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.haji.Restfulwebservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfulWebServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
